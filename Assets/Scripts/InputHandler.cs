250ad03b8f15fa4b1a49bccb950df1f409323b93
Jerri Ahonen <jerri.ahonen@greenergrass.company>
2021-07-30 11:55:24 +0300

    Add Input Handler

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class InputHandler : SingletonBehaviour<InputHandler>
+{
+	public bool IsAttacking { get; private set; }
+
+	public event Action Attack;
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Attack?.Invoke();
+			IsAttacking = true;
+		}
+		
+	}
+}
