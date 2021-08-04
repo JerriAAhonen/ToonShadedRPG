d98067bd93967b35dd64963320d13c7547fd028d
Jerri Ahonen <jerri.ahonen@greenergrass.company>
2021-08-04 13:14:28 +0300

    Convert indents to tabs

--- Assets/Scripts/InteractableTree.cs
+++ Assets/Scripts/InteractableTree.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class InteractableTree : MonoBehaviour
 {
-    private Rigidbody rb;
-    
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        rb.constraints = RigidbodyConstraints.FreezeAll;
-    }
+	private Rigidbody rb;
+	private int maxHealth = 100;
+	private int health;
+	
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.constraints = RigidbodyConstraints.FreezeAll;
+		health = maxHealth;
+	}
 
-    public void Cut(Vector3 hitForce)
-    {
-        rb.constraints = RigidbodyConstraints.None;
-        rb.AddForce(hitForce);
-    }
+	public void Cut(Vector3 hitForce)
+	{
+		health -= 30;
+		if (health > 0)
+		{
+			StartCoroutine(ShakeRoutine());
+			return;   
+		}
+		
+		rb.constraints = RigidbodyConstraints.None;
+		rb.AddForce(hitForce);
+	}
+
+	private IEnumerator ShakeRoutine()
+	{
+		
+	}
 }
