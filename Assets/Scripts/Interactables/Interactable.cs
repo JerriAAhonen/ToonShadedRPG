643f4837a6efe73b9a4ab0d106dcc3921ae2bcd3
Jerri Ahonen <jerri.ahonen@greenergrass.company>
2021-08-05 14:43:30 +0300

    Add interactables

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+	string DisplayName { get; }
+}
+
+public abstract class Interactable : MonoBehaviour, IInteractable
+{
+	public virtual string DisplayName { get; }
+}
---
+++ Assets/Scripts/Interactables/Lumber.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering.Universal.ShaderGUI;
+using UnityEngine;
+
+public class Lumber : Interactable
+{
+	public override string DisplayName { get; } = "Wood";
+}
