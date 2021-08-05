5dd1316dd63a07197d21170f0378fd3e4d7d7402
Jerri Ahonen <jerri.ahonen@greenergrass.company>
2021-08-05 14:43:14 +0300

    Add camera raycaster for interacting with the world

---
+++ Assets/Scripts/CameraRaycaster.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycaster : MonoBehaviour
+{
+	private bool tooltipActive;
+	private float reachDistance = 10f;
+	
+	private void Update()
+	{
+		var ray = new Ray(transform.position, transform.forward);
+		if (Physics.Raycast(ray, out var hit, reachDistance))
+		{
+			var interactable = hit.collider.GetComponent<Interactable>();
+			if (interactable != null)
+			{
+				CrosshairTooltips.Instance.ShowTargetDisplayName(interactable.DisplayName);
+				tooltipActive = true;
+			}
+		}
+		else if (tooltipActive)
+		{ 
+			CrosshairTooltips.Instance.Hide(); 
+			tooltipActive = false;
+		}
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawRay(transform.position, transform.forward * reachDistance);
+	}
+}
