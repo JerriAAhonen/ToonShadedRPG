55270441e1094ad1cad0b9cd21d6424ee00848af
Jerri Ahonen <jerri.ahonen@greenergrass.company>
2021-09-12 19:30:39 +0300

    Add Vector Utils helper class

---
+++ Assets/Scripts/Util/VectorUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VectorUtils
+{
+	public static Vector3 GetDirVector(Vector3 from, Vector3 to)
+	{
+		return (to - from).normalized;
+	}
+}
