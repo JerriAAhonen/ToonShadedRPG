ac676a6287508e95ef20ade83f740b5e5a0fdd78
Jerri Ahonen <jerri.ahonen@gmail.com>
2021-11-06 11:22:26 +0200

    Add enemySpawner

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+	public GameObject enemyPrefab;
+	public Transform spawnPoint;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+}
