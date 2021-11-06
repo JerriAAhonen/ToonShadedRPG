6abedb73085dd3430c63b70f8a4d25e4f24ed27e
Jerri Ahonen <jerri.ahonen@gmail.com>
2021-11-06 11:17:37 +0200

    Add hit particle effect

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -6,12 +6,20 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public float hitForce;
+    public ParticleSystem hitEffect;
 
     private void OnTriggerEnter(Collider other)
     {
         var enemy = other.GetComponent<Enemy>();
         if (enemy != null)
+        {
+            if (hitEffect != null)
+            {
+                var effect = Instantiate(hitEffect, other.ClosestPoint(transform.position), Quaternion.identity);
+                effect.Play(true);
+            }
             HitEnemy(enemy);
+        }
 
         var tree = other.GetComponent<InteractableTree>();
         if (tree != null)
