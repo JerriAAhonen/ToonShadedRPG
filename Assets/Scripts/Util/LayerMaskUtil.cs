266ea3692cc6b5a7581131bfc1509b09d3f3e597
Jerri Ahonen <jerri.ahonen@gmail.com>
2021-11-20 00:06:31 +0200

    Add util for layermasks

---
+++ Assets/Scripts/Util/LayerMaskUtil.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Util
+{
+	public static class LayerMaskUtil
+	{
+		public static bool IsLayerInMask(LayerMask mask, int layer)
+		{
+			return mask == (mask | (1 << layer));
+		}
+	}
+}
