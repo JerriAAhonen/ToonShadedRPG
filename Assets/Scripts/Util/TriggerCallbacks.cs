ae0a849ac7c15351fab43170c6d6b6abc9d11657
Jerri Ahonen <jerri.ahonen@gmail.com>
2021-11-19 18:26:26 +0200

    Add triggerCallbacks.cs

---
+++ Assets/Scripts/Util/TriggerCallbacks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Util
+{
+	public class TriggerCallbacks : MonoBehaviour
+	{
+		[SerializeField] private LayerMask mask;
+
+		public event Action<Collider> Enter;
+		public event Action<Collider> Exit;
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (LayerMaskUtil.IsLayerInMask(mask, other.gameObject.layer))
+				Enter?.Invoke(other);
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			if (LayerMaskUtil.IsLayerInMask(mask, other.gameObject.layer))
+				Exit?.Invoke(other);
+		}
+	}
+}
